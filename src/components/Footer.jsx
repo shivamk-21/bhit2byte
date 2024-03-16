48d26c2d11848910d630676ec6941c12c35ab165
vanshgoel123 <vanshg247776@gmail.com>
2024-03-16 20:01:35 +0530

    New Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const components = () => {
   return(
     <div className="footer">
-      <h2>Contact US</h2>
+      <h2>Contact Us</h2>
       <h3>Back to Top</h3>
     </div>
   )
